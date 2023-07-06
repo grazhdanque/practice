2a4d259de549b11faef5ad6836f07644a03c0d94
Artyom <hubabuba0144@gmail.com>
2023-07-06 23:41:15 +0300

    Добавили task04

---
+++ task04/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a == b && b == c)
+{
+ Console.WriteLine("Все числа равны");
+}
+int max = a;
+if (max < b)  max = b;
+if (max < c)  max = c;
+Console.WriteLine($"мах = {max}");
