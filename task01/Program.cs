5cc7969376dda41abdee3a78576ec4b0f5f9c365
Artyom <hubabuba0144@gmail.com>
2023-07-06 22:33:41 +0300

    Добавили task01

---
+++ task01/Program.cs
@@ -0,0 +1,11 @@
+﻿int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2*num2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
