be273a1cfae7e81b69e17087c3cba18c3e9bc11d
Artyom <hubabuba0144@gmail.com>
2023-07-07 00:09:42 +0300

    Добавили task08

---
+++ task08/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое натуральное число");
+double num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+if (num <= 0) 
+{
+    Console.WriteLine("Число не является натуральным");
+}
+else while (count != num + 1)
+{
+    Console.Write($"{count} ");
+    count += 1;
+}
