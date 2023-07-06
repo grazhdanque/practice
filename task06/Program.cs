262b8bc0adb24b514848328a767ab4efcc62140e
Artyom <hubabuba0144@gmail.com>
2023-07-06 23:54:04 +0300

    Добавили task06

---
+++ task06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int div = num % 2;
+if (div == 0)
+{
+    Console.WriteLine("Да");
+}  
+else
+{
+    Console.WriteLine("Нет");
+}
