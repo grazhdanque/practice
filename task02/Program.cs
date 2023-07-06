432a953031e80cdbce2d2d766b5fbfdf2eb248fd
Artyom <hubabuba0144@gmail.com>
2023-07-06 23:31:48 +0300

    Добавили task02

---
+++ task02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else
+{
+    Console.WriteLine($"max = {b}");
+}
+if (a == b)
+{
+    Console.WriteLine("Числа равны");
+}
