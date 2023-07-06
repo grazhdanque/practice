618009a2b2e1c80f1b1e7d7c47eeb7a5e3f64fad
Artyom <hubabuba0144@gmail.com>
2023-07-06 23:21:59 +0300

    Добавили task07

---
+++ task07/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0) 
+{
+    num *= -1;
+}
+if (num >= 100 && num <= 999)
+{
+Console.WriteLine($"{num} -> {num % 10}");
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
