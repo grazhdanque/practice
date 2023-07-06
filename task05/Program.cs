79ab45b9ed67ccf24eb0d3632d7ae3d89f77600a
Artyom <hubabuba0144@gmail.com>
2023-07-06 23:08:59 +0300

    Добавили task05

---
+++ task05/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{num} ->");
+int numrev = (num * -1);
+while (numrev != num)
+{
+    Console.Write($"{numrev++}, ");
+}
+Console.WriteLine(num);
