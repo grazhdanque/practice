1347ac85895f660632b7c0c11f6b5393c010b2c5
Artyom <hubabuba0144@gmail.com>
2023-07-06 22:55:37 +0300

    Добавили task03

---
+++ task03/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1) 
+{
+    Console.WriteLine($"{num} - Понедельник");
+}
+else if (num == 2) 
+{
+    Console.WriteLine($"{num} - Вторник");
+}
+else if (num == 3) 
+{
+    Console.WriteLine($"{num} - Среда");
+}
+else if (num == 4) 
+{
+    Console.WriteLine($"{num} - Четверг");
+}
+else if (num == 5) 
+{
+    Console.WriteLine($"{num} - Пятница");
+}
+else if (num == 6) 
+{
+    Console.WriteLine($"{num} - Суббота");
+}
+else if (num == 7) 
+{
+    Console.WriteLine($"{num} - Воскресенье");
+}
+else  
+{
+    Console.WriteLine($"{num} - неверное значение дня недели");
+}
